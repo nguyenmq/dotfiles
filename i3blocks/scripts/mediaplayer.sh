204c1f5fbe8e479c827edbf59381af91864a6701
Minh Quan Nguyen <minhquanng08@gmail.com>
2017-01-06 23:51:41 -0800

    xml encode strings in mediaplayer.sh

--- i3blocks/scripts/mediaplayer.sh
+++ i3blocks/scripts/mediaplayer.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+utf8_to_xml_encoding() {
+    echo "$1" | recode utf8..xml
+}
+
 if [ $(playerctl -p mpd status) == "Playing" ]; then
-    echo "<span foreground='#e3e2f3'>  </span>  $(mpc current)"
+    echo "<span foreground='#e3e2f3'>  </span>  $(utf8_to_xml_encoding "$(mpc current)")"
 elif [ $(playerctl -p spotify status) == "Playing" ]; then
-    echo "<span foreground='#e3e2f3'>  </span>  $(playerctl -p spotify metadata artist) - $(playerctl -p spotify metadata title)"
+    echo "<span foreground='#e3e2f3'>  </span>  $(utf8_to_xml_encoding "$(playerctl -p spotify metadata artist) - $(playerctl -p spotify metadata title)")"
 elif [ $(playerctl -p mpd status) == "Paused" ]; then
-    echo "<span foreground='#e3e2f3'>  </span>  $(mpc current)"
+    echo "<span foreground='#e3e2f3'>  </span>  $(utf8_to_xml_encoding "$(mpc current)")"
 elif [ $(playerctl -p spotify status) == "Paused" ]; then
-    echo "<span foreground='#e3e2f3'>  </span>  $(playerctl -p spotify metadata artist) - $(playerctl -p spotify metadata title)"
+    echo "<span foreground='#e3e2f3'>  </span>  $(utf8_to_xml_encoding "$(playerctl -p spotify metadata artist) - $(playerctl -p spotify metadata title)")"
 else
     echo "  stopped"
 fi
