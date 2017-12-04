ee04dc0e6d43794dbbd3f4609f9887bb0371864d
Minh Quan Nguyen <minhquanng08@gmail.com>
2017-12-03 23:14:03 -0800

    Set fallback text in wifi strength script

--- i3blocks/scripts/wifi.sh
+++ i3blocks/scripts/wifi.sh
@@ -6,12 +6,9 @@ INTERFACE="${BLOCK_INSTANCE:-wlan0}"
 
 # As per #36 -- It is transparent: e.g. if the machine has no battery or wireless
 # connection (think desktop), the corresponding block should not be displayed.
-[[ ! -d /sys/class/net/${INTERFACE}/wireless ]] ||
-    [[ "$(cat /sys/class/net/$INTERFACE/operstate)" = 'down' ]] && exit
-
-#------------------------------------------------------------------------
-
-QUALITY=$(grep $INTERFACE /proc/net/wireless | awk '{ print int($3 * 100 / 70) }')
-
-echo $QUALITY% # full text
-echo $QUALITY% # short text
+if [[ ! -d /sys/class/net/${INTERFACE}/wireless ]] || [[ "$(cat /sys/class/net/$INTERFACE/operstate)" = 'down' ]]; then
+    echo ":("
+else
+    QUALITY=$(grep $INTERFACE /proc/net/wireless | awk '{ print int($3 * 100 / 70) }')
+    echo $QUALITY% # full text
+fi
