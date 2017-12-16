46ce006465058ed92876b25a2c6f818863f171cc
Minh Quan Nguyen <minhquanng08@gmail.com>
2017-12-16 15:34:00 -0800

    Switch desktop to polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -11,5 +11,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar "${1}" &
+# launch polybar on each output
+for m in $(xrandr --query | awk '/\<connected\>/ {print $1}'); do
+    MONITOR=$m polybar "$1" &
+done
