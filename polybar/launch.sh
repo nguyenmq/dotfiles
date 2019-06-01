efabd312ffb18b12b6059c1ffd31e02647557988
Minh Quan Nguyen <minhquanng08@gmail.com>
2019-06-01 10:17:44 -0700

    polybar launch script and i3 desktop config
    - Update the polybar launch script to always exec polybar on the primary
      monitor first so that the application trays ends up there
    - Use nm-applet
    - Minor tweaks to i3 desktop config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
 if [[ $# -lt 2 ]]; then
-    echo "Need to specify a polybar config and network device"
+    echo "Need to specify a polybar config name and network device"
     exit 1
 fi
+
+function launch_polybar() {
+    local monitor=$1
+    local network=$2
+    local config_name=$3
+
+    MONITOR="$monitor" NETWORK="$network" polybar "$config_name" &
+    sleep 0.2s
+}
+
+CONFIG_NAME="$1"
+NETWORK="$2"
 
 # Terminate already running bar instances
 killall -q polybar
@@ -11,8 +23,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# launch polybar on each active output
-for m in $(xrandr --query | awk '/\<connected\> (primary )?[0-9]/ {print $1}' | sort --reverse); do
-    MONITOR=$m NETWORK="$2" polybar "$1" &
-    sleep 0.2s
+# Launch polybar on the primary monitor first. This is where the application tray will appear.
+PRIMARY=$(xrandr --query | awk '/\<connected primary/ {print $1}')
+launch_polybar "$PRIMARY" "$NETWORK" "$CONFIG_NAME"
+
+# Launch polybar on the remaining active monitors
+for m in $(xrandr --query | awk '/\<connected\> [0-9]/ {print $1}'); do
+    launch_polybar "$m" "$NETWORK" "$CONFIG_NAME"
 done
