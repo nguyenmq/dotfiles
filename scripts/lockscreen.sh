d6ad5a9b2c97aad91e8030ded065f4c89e5a7d3c
Minh Quan Nguyen <minhquanng08@gmail.com>
2020-01-19 12:32:39 -0800

    Set objects on lockscreen relative to origin location

--- scripts/lockscreen.sh
+++ scripts/lockscreen.sh
@@ -3,28 +3,38 @@
 # to create a dim image:
 # convert 20170604_0110.png -fill black -colorize 40% .xlock/lock_dim.png
 
+ORIGIN_MM=22 # mm
+SPACING=8 # mm
+RADIUS=40 # px
+DATE_SIZE=20 # px
+TIME_SIZE=38 # px
 NORMAL_DPI=96
 
 current_dpi=$(xrdb -query | awk '/Xft.dpi/ {print $2}')
-
-if [[ -n "$current_dpi" ]] && [[ "$current_dpi" -gt "$NORMAL_DPI" ]]; then
-    indpos=$(xrandr | awk '/connected primary/ {split($4, res, "x|+"); printf "%d:%d", res[1]*0.035, res[2]*0.43}')
-    timepos=$(xrandr | awk '/connected primary/ {split($4, res, "x|+"); printf "%d:%d", res[1]*0.06, res[2]*0.43}')
-    datepos=$(xrandr | awk '/connected primary/ {split($4, res, "x|+"); printf "%d:%d", res[1]*0.06, res[2]*0.45}')
-else
-    indpos=$(xrandr | awk '/connected primary/ {split($4, res, "x|+"); printf "%d:%d", res[1]*0.05, res[2]*0.8}')
-    timepos=$(xrandr | awk '/connected primary/ {split($4, res, "x|+"); printf "%d:%d", res[1]*0.085, res[2]*0.8}')
-    datepos=$(xrandr | awk '/connected primary/ {split($4, res, "x|+"); printf "%d:%d", res[1]*0.085, res[2]*0.82}')
+if [[ -z "$current_dpi" ]]; then
+    current_dpi="$NORMAL_DPI"
 fi
 
+scale_factor=$(( current_dpi / NORMAL_DPI ))
+px_per_mm=$(xrandr | awk -v "scale=$scale_factor" '/connected primary/ {split($4, resolution, "x|+"); real_width=substr($NF, 0, length($NF) - 2); printf "%d", ((resolution[2] / scale) / real_width)}')
+
+origin=$(( ORIGIN_MM * px_per_mm ))
+text_spacing="$RADIUS"
+left_text=$(( text_spacing + origin + RADIUS ))
+time_ypos=$(( origin - ( RADIUS / 4 ) ))
+date_ypos=$(( time_ypos + TIME_SIZE  ))
+
+indpos="${origin}:${origin}"
+timepos="${left_text}:${time_ypos}"
+datepos="${left_text}:${date_ypos}"
 
 /usr/bin/i3lock \
     --tiling --image="${HOME}/.xlock/lock_dim.png" \
     --ignore-empty-password \
     --force-clock \
-    --indicator --indpos="$indpos" --radius=40 \
-    --date-align=1 --datestr="%A  %Y/%m/%d" --datepos="$datepos" --date-font="Ubuntu" --datesize=20 --datecolor=ffffffff \
-    --time-align=1 --timestr="%I:%M %p" --timepos="$timepos" --time-font="Ubuntu" --timesize=38 --timecolor=ffffffff \
+    --indicator --indpos="$indpos" --radius="$RADIUS" \
+    --date-align=1 --datestr="%A  %Y/%m/%d" --datepos="$datepos" --date-font="Ubuntu" --datesize="$DATE_SIZE" --datecolor=ffffffff \
+    --time-align=1 --timestr="%I:%M %p" --timepos="$timepos" --time-font="Ubuntu" --timesize="$TIME_SIZE" --timecolor=ffffffff \
     --insidecolor=00000000 \
     --insidevercolor=00000000 \
     --insidewrongcolor=00000000 \
