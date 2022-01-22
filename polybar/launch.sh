b8a4f9dd0b6c2a07b247f10c23015f25afab2731
Minh Quan Nguyen <minhquanng08@gmail.com>
2022-01-22 01:36:21 -0800

    Initial migration to dotter
    Convert config files to use templating supported by dotter

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/env bash
 
-if [[ $# -lt 2 ]]; then
-    echo "Need to specify a polybar config name and network device"
-    exit 1
-fi
-
 function launch_polybar() {
     local monitor=$1
-    local network=$2
-    local config_name=$3
 
-    MONITOR="$monitor" NETWORK="$network" polybar "$config_name" &
+    MONITOR="$monitor" polybar "base" &
     sleep 0.2s
 }
-
-CONFIG_NAME="$1"
-NETWORK="$2"
 
 # Terminate already running bar instances
 killall -q polybar
@@ -26,9 +16,9 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar on the primary monitor first. This is where the application tray will appear.
 PRIMARY=$(xrandr --query | awk '/\<connected primary/ {print $1}')
-launch_polybar "$PRIMARY" "$NETWORK" "$CONFIG_NAME"
+launch_polybar "$PRIMARY"
 
 # Launch polybar on the remaining active monitors
 for m in $(xrandr --query | awk '/\<connected\> [0-9]/ {print $1}'); do
-    launch_polybar "$m" "$NETWORK" "$CONFIG_NAME"
+    launch_polybar "$m"
 done
