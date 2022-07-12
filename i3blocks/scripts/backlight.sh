0b86bf58da12a1d8633dbdc5b3d2973eab5ac456
Minh Quan Nguyen <minhquanng08@gmail.com>
2022-07-11 23:56:51 -0700

    remove i3blocks

--- i3blocks/scripts/backlight.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-BRIGHTNESS=$(printf %.0f "$(xbacklight -get)")
-echo "<span foreground='#e3e2f3'></span>  $BRIGHTNESS%"
--- i3blocks/scripts/ethernet.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if  [[ $(cat /sys/class/net/$BLOCK_INSTANCE/dormant) -ne 0 ]]; then
-    echo "No Connection"
-else
-    echo "  $(hostname -i)"
-fi
