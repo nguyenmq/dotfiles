2bcdc3a3b3a53d7a3306c00d6d385a2af55f6961
Minh Quan Nguyen <minhquanng08@gmail.com>
2022-07-11 17:59:57 -0700

    Fix surfingkey method calls that were moved to the api namespace

--- surfingkeys/surfingkeys.js
+++ surfingkeys/surfingkeys.js
@@ -7,37 +7,40 @@ settings.richHintsForKeystroke = false;
 settings.modeAfterYank = 'Normal';
 
 // new mappings
-mapkey('p', 'Switch frames', 'Normal.rotateFrame()');
-mapkey('s', '#2Scroll down', 'Normal.scroll("down")', {repeatIgnore: true});
-mapkey('w', '#2Scroll up', 'Normal.scroll("up")', {repeatIgnore: true});
-mapkey('gu', '#4Edit current URL with vim editor', function() {
-    Front.showEditor(window.location.href, function(data) {
+api.mapkey('p', 'Switch frames', 'Normal.rotateFrame()');
+api.mapkey('s', '#2Scroll down', 'Normal.scroll("down")', {repeatIgnore: true});
+api.mapkey('w', '#2Scroll up', 'Normal.scroll("up")', {repeatIgnore: true});
+api.mapkey('gu', '#4Edit current URL with vim editor', function() {
+    api.Front.showEditor(window.location.href, function(data) {
         RUNTIME("openLink", { tab: { tabbed: false }, url: data } );
     }, 'url');
 });
-mapkey('gU', '#4Edit current URL with vim editor', function() {
-    Front.showEditor(window.location.href, function(data) {
+api.mapkey('gU', '#4Edit current URL with vim editor', function() {
+    api.Front.showEditor(window.location.href, function(data) {
         tabOpenLink(data);
     }, 'url');
 });
 
 // unmaps
-unmap('C');
-unmap('ss');
-unmap('k', /(reddit|feedly|mail\.google)\.com/);
-unmap('j', /(reddit|feedly|mail\.google)\.com/);
-unmap('v', /feedly\.com/);
-unmap('p', /reddit\.com/);
-unmap('m', /reddit\.com/);
-unmap('M', /reddit\.com/);
-unmap('n', /reddit\.com/);
-iunmap(':');
+api.unmap('C');
+api.unmap('ss');
+api.unmap('k', /(reddit|feedly|mail\.google)\.com/);
+api.unmap('j', /(reddit|feedly|mail\.google)\.com/);
+api.unmap('v', /feedly\.com/);
+api.unmap('p', /reddit\.com/);
+api.unmap('m', /reddit\.com/);
+api.unmap('M', /reddit\.com/);
+api.unmap('n', /reddit\.com/);
+api.iunmap(':');
 
 // alias current mappings
-map('F', 'af');
-map('s', 'j');
-map('w', 'k');
-map(';m', 'sm');
+api.map('F', 'af');
+api.map('s', 'j');
+api.map('w', 'k');
+api.map(';m', 'sm');
+
+// link hint color
+api.Hints.style('border: solid 1px #000000; color:#000000; background: initial; background-color: #fbff6f; font-family: "Ubuntu";');
 
 // omnibar theme
 settings.theme = `
@@ -45,25 +48,29 @@ settings.theme = `
     background: #ffffff;
     color: #000000;
 }
+
 .sk_theme tbody {
     color: #ffffff;
 }
+
 .sk_theme input {
     color: #000000;
 }
+
 .sk_theme .url {
     color: #2bb24c;
 }
+
 .sk_theme .annotation {
     color: #000000;
 }
 
 .sk_theme .focused {
     background: #c7c7c7;
-};
+}
 
-.sk_hints {
-    background: #2bb24c !important;
-};
+#sk_status {
+    right: 89% !important;
+    font-size: 14px !important;
+}
 `
-
