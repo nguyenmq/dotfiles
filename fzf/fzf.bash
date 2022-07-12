19f9dca9f29dc0ebf6459fc0036d91af81b2e20a
Minh Quan Nguyen <minhquanng08@gmail.com>
2022-07-11 23:54:51 -0700

    Remove unused fzf import

--- fzf/fzf.bash
+++
@@ -1,7 +0,0 @@
-if [[ -f "/usr/share/fzf/completion.bash" ]]; then
-    source "/usr/share/fzf/completion.bash"
-fi
-
-if [[ -f "/usr/share/fzf/key-bindings.bash" ]]; then
-    source "/usr/share/fzf/key-bindings.bash"
-fi
