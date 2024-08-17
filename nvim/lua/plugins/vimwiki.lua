cf094452f0bf094abe3cee2f6dcbbb9815f3b0d1
Minh Quan Nguyen <minhquanng08@gmail.com>
2024-08-16 17:33:19 -0700

    nvim, vim-wiki: add keymap to remove done tasks

--- nvim/lua/plugins/vimwiki.lua
+++ nvim/lua/plugins/vimwiki.lua
@@ -11,5 +11,8 @@ return {
         }
         vim.g.vimwiki_list_margin = 0
         vim.g.vimwiki_listsyms = ' .:ox'
+    end,
+    config = function()
+        vim.keymap.set('v', '<Leader>wrm', '<cmd>VimwikiRemoveDone<cr>')
     end
 }
