7fbd2898a0e4d47b40a0c121a11de7efcd55e776
Minh Quan Nguyen <minhquanng08@gmail.com>
2024-11-22 15:54:09 -0800

    nvim: telescope bindings
    - Prefer to use lsp client if available for symbol matching. Else
      fallback to ctags.
    - Add bindings for search for lsp refernces and incoming calls

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -36,6 +36,12 @@ return {
                     only_sort_tags = true,
                     fname_width = 50,
                     ctags_file = tags_file,
+                },
+                diagnostics = {
+                    bufnr = 0,
+                },
+                lsp_document_symbols = {
+                    fname_width = 50,
                 }
             }
         }
@@ -46,12 +52,28 @@ return {
         vim.keymap.set('n', '<leader>S', extensions.live_grep_args.live_grep_args, {})
         vim.keymap.set('n', '<leader>b', builtin.buffers, {})
         vim.keymap.set('n', '<leader>r', builtin.command_history, {})
-        vim.keymap.set('n', '<leader>T', builtin.tags, {})
-        vim.keymap.set('n', '<leader>t', function()
-            -- run ctags on the current buffer to get an updated tags file
-            local filename = vim.fn.expand('%:p')
-            os.execute('ctags -o ' .. tags_file .. ' ' .. filename)
-            builtin.current_buffer_tags()
+        vim.keymap.set('n', '<leader>T', function()
+            local lsp_count = table.getn(vim.lsp.get_clients({bufnr=0}))
+            if lsp_count > 0 then
+                builtin.lsp_workspace_symbols()
+            else
+                builtin.tags()
+            end
         end)
+        vim.keymap.set('n', '<leader>t', function()
+            local lsp_count = table.getn(vim.lsp.get_clients({bufnr=0}))
+
+            if lsp_count > 0 then
+                builtin.lsp_document_symbols({fname_width=50})
+            else
+                -- run ctags on the current buffer to get an updated tags file if no lsp client
+                local filename = vim.fn.expand('%:p')
+                os.execute('ctags -o ' .. tags_file .. ' ' .. filename)
+                builtin.current_buffer_tags()
+            end
+        end)
+        vim.keymap.set('n', '<leader>d', function() builtin.diagnostics({bufnr = 0}) end)
+        vim.keymap.set('n', '<leader>lr', function() builtin.lsp_references({}) end)
+        vim.keymap.set('n', '<leader>lc', function() builtin.lsp_incoming_calls({}) end)
     end,
 }
