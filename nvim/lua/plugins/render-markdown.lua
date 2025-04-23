75ade871467541cb46836a7de8ed8ea642a619c1
Minh Quan Nguyen <minhquanng08@gmail.com>
2025-04-23 16:09:01 -0700

    nvim(render-markdown): fix breaking change for ordered lists
    In version 8.0, render-markdown changed the function prototype for
    custom functions in the config, which broke the function I copied for
    handling ordered lists. This is the default function so removing the
    custom one entirely.

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -152,23 +152,7 @@ return {
             bullet = {
                 -- Turn on / off list bullet rendering
                 enabled = true,
-                -- Replaces '-'|'+'|'*' of 'list_item'
-                -- How deeply nested the list is determines the 'level', how far down at that level determines the 'index'
-                -- If a function is provided both of these values are passed in using 1 based indexing
-                -- If a list is provided we index into it using a cycle based on the level
-                -- If the value at that level is also a list we further index into it using a clamp based on the index
-                -- If the item is a 'checkbox' a conceal is used to hide the bullet instead
                 icons = { '' },
-                -- Replaces 'n.'|'n)' of 'list_item'
-                -- How deeply nested the list is determines the 'level', how far down at that level determines the 'index'
-                -- If a function is provided both of these values are passed in using 1 based indexing
-                -- If a list is provided we index into it using a cycle based on the level
-                -- If the value at that level is also a list we further index into it using a clamp based on the index
-                ordered_icons = function(level, index, value)
-                    value = vim.trim(value)
-                    local value_index = tonumber(value:sub(1, #value - 1))
-                    return string.format('%d.', value_index > 1 and value_index or index)
-                end,
                 -- Padding to add to the left of bullet point
                 left_pad = 1,
                 -- Padding to add to the right of bullet point
